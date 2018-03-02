60eb809fb65464b1e6650525398f79dec71a6032
Hubert Kompanowski <hubert.kompanowski9@gmail.com>
2018-03-02 21:50:07 +0100

    5/6

---
+++ lab1/multiplicationtable/MultiplicationTable.h
@@ -0,0 +1,14 @@
+//
+// Created by hubert on 02.03.18.
+//
+
+#ifndef JIMP_EXERCISES_MULTIPLICATIONTABLE_H
+#define JIMP_EXERCISES_MULTIPLICATIONTABLE_H
+
+
+
+void MultiplicationTable(int tab[][10]);
+void drawtable(int tab[][10]);
+
+
+#endif //JIMP_EXERCISES_MULTIPLICATIONTABLE_H
---
+++ lab1/multiplicationtable/main.cpp
@@ -0,0 +1,15 @@
+//
+// Created by hubert on 02.03.18.
+//
+
+
+#include <iostream>
+#include "MultiplicationTable.h"
+
+int main() {
+    int tab[10][10];
+    MultiplicationTable(tab);
+    drawtable(tab);
+
+    return 0;
+}
