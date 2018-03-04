b470ef444e03d2b9f591073941a63d5ccacd39a8
abrajner <ala.brajner@gmail.com>
2018-03-04 12:13:56 +0100

    reversestring

--- lab1/reversestring/ReverseString.cpp
+++ lab1/reversestring/ReverseString.cpp
@@ -1,9 +1,19 @@
 //
 // Created by komphube on 27.02.18.
 //
-
+#include <string>
 #include "ReverseString.h"
 
 std::string reverse(std::string str) {
-    return 0;
-}
+    size_t size = str.length();
+    std::string reversed_characters;
+
+    if(size <= 1){
+        return str;
+    }
+
+    char last_character = str[size-1];
+    str.erase((size-1), 1);
+    reversed_characters = last_character + reverse(str);
+    return std::string(reversed_characters);
+    }
