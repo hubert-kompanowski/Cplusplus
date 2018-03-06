39dce56ba75697b90fe3d6fbbd2043dd37a3e8a0
abrajner <ala.brajner@gmail.com>
2018-03-06 10:09:25 +0100

    refactor

---
+++ lab2/array2d/Array2D.cpp
@@ -0,0 +1,24 @@
+//
+// Created by abrajner on 05.03.18.
+//
+
+#include <iostream>
+#include "Array2d.h"
+
+using namespace std;
+
+void UserInput(int n_rows, int n_columns){
+    cout << "podaj liczbę kolumn ";
+    cin >> n_columns;
+    cout << endl << "Podaj liczbe wierszy ";
+    cin >> n_rows;
+    cout << endl;
+}
+int ** Array2D(int n_rows, int n_columns){
+
+}
+
+void DeleteArray2D(int ** array, int n_rows, int n_columns){
+
+}
+
---
+++ lab2/array2d/Array2D.h
@@ -0,0 +1,12 @@
+//
+// Created by abrajner on 05.03.18.
+//
+
+#ifndef JIMP_EXERCISES_ARRAY2D_H
+#define JIMP_EXERCISES_ARRAY2D_H
+
+void UserInput(int n_rows, int n_columns);
+
+
+
+#endif //JIMP_EXERCISES_ARRAY2D_H
